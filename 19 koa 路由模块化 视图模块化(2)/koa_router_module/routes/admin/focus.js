a8f77a66a7f57c5daf4a1ee292ec403dc1f57034
liuchenyang0515 <897418643@qq.com>
2020-11-24 19:45:22 +0800

    Koa路由模块化--视图模块化

---
+++ 19 koa 路由模块化 视图模块化(2)/koa_router_module/routes/admin/focus.js
@@ -0,0 +1,29 @@
+/*轮播图的增加修改删除*/
+
+
+var router=require('koa-router')();
+
+router.get('/',async (ctx)=>{
+
+    await  ctx.render('admin/focus/index');
+})
+
+router.get('/add', async (ctx)=>{
+
+    await  ctx.render('admin/focus/add');
+
+})
+
+
+router.get('/edit',async (ctx)=>{
+
+    await  ctx.render('admin/focus/edit');
+
+})
+router.get('/delete',async (ctx)=>{
+
+    ctx.body='删除轮播图';
+
+})
+
+module.exports=router.routes();
