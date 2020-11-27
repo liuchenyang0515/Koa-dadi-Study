bb8639a5b5f543c3f6b89113415d1fcbc0f99252
liuchenyang0515 <897418643@qq.com>
2020-11-27 16:04:26 +0800

    引入silly-datetime控制时间格式，以及数据库成员管理员身份控制

---
+++ 26 Koa ArtTemplate 自定义日期管道 封装Ajax改变状态的公用方法 koa-jsonp实现api接口/cms_demo07/public/admin/basic.js
@@ -0,0 +1,13 @@
+const app = {
+    toggle: function (el, collectionName, attr, id) {
+        $.get('/admin/changeStatus', {collectionName: collectionName, attr: attr, id: id}, function (data) {
+            if (data.success) {
+                if (el.src.indexOf('yes') != -1) {
+                    el.src = '/admin/images/no.gif';
+                } else {
+                    el.src = '/admin/images/yes.gif';
+                }
+            }
+        })
+    }
+}
---
+++ 26 Koa ArtTemplate 自定义日期管道 封装Ajax改变状态的公用方法 koa-jsonp实现api接口/cms_demo07/routes/admin/manage.js
@@ -0,0 +1,24 @@
+const router = require('koa-router')();
+const DB = require('../../model/db.js');
+
+router.get('/', async (ctx) => {
+    var result = await DB.find('admin', {});
+    console.log(result);
+    await ctx.render('admin/manage/list', {
+        list: result
+    });
+})
+
+router.get('/add', async (ctx) => {
+    await ctx.render('admin/manage/add');
+})
+
+router.get('/edit', async (ctx) => {
+    ctx.body = "编辑用户";
+})
+
+router.get('/delete', async (ctx) => {
+    ctx.body = "删除用户";
+})
+
+module.exports = router.routes();
